7fe097374652ed49924fe28fe16840372b25d32b
Pinguin <maxsunkovsky@gmail.com>
2023-12-19 19:46:30 +0100

    push repo to GitHub

---
+++ iss.py
@@ -0,0 +1,32 @@
+import requests
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+
+def track_iss():
+    reply = requests.get("http://api.open-notify.org/iss-now.json")
+    if reply.status_code == 200:
+        data = reply.json()
+        iss_pos = data["iss_position"]
+        return iss_pos
+    else:
+        return None
+    
+def iss_map():
+    iss_pos = track_iss()
+    if iss_pos:
+        lon = float(iss_pos['longitude'])
+        lat = float(iss_pos['latitude'])
+        
+        fig = plt.figure(figsize=(8, 8))
+        ax = plt.axes(projection=ccrs.Orthographic(central_longitude=lon, central_latitude=lat))
+        ax.coastlines()
+        ax.gridlines(draw_labels=False)
+        ax.set_title("Current position of the ISS")
+        plt.show() 
+        ax.plot(lon, lat, 'ro', markersize=10, transform=ccrs.PlateCarree()) 
+        
+    else:
+        print("Couldn't get information about the ISS position")
+
+if __name__ == "__main__":
+    iss_map()
