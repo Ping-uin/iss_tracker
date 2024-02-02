c274dabb78134b5886c412dce5ba65f0fb5abca6
Pinguin <maxsunkovsky@gmail.com>
2024-02-02 19:02:50 +0100

    Closing bug fixed!

--- iss.py
+++ iss.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import time
 
+close_state = False
+
 
 def track_iss():
     reply = requests.get("http://api.open-notify.org/iss-now.json")
@@ -14,14 +16,24 @@ def track_iss():
         return None
 
 
+def on_close(event):
+    global close_state
+    close_state = True
+
+
 def iss_map():
+    global close_state
     fig = plt.figure(figsize=(8, 8))
     ax = plt.axes(projection=ccrs.PlateCarree())
     ax.stock_img()
     (line,) = plt.plot([], [], color="blue", marker="o")
     ax.set_title("Current position of the ISS")
+    # fig.show()
+    close_state = False
 
     while True:
+        if close_state:
+            break
         iss_pos = track_iss()
         if iss_pos:
             lon = float(iss_pos["longitude"])
@@ -29,10 +41,11 @@ def iss_map():
             line.set_data(lon, lat)
             plt.pause(1)  # Pause for 1 second to update the plot
             plt.draw()  # Redraw the plot
+            fig.canvas.mpl_connect("close_event", on_close)
         else:
             print("Couldn't get information about the ISS position")
             break
-    plt.show()
+    # plt.show()
 
 
 if __name__ == "__main__":
